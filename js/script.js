ca176a78ef45fc9dc110aa2f995e9213d6d55a79
Andrea Creazzi <andreacreazzi@libero.it>
2023-06-05 14:23:28 +0200

    add html-js (complete)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
 // controllo js
 console.log(`JS OK`)
+
+// elemento preso dal DOM
+const listElement = document.getElementById(`list`);
+console.log(listElement)
+
+// array dalla lista
+
+const items = [`latte,` , `yogurt,` , `pane,` , `ecc`];
+console.log(items)
+// variabile html
+let item;
+
+// ciclo for
+// for(let i = 0 ; i < items.length ; i++){
+//     item = items[i]
+//     console.log(item)
+//     ! elemento inserito nel DOM
+//     listElement.innerText += item
+// };
+
+// ciclo while
+
+let i = items
+ while(!item){
+    item = i
+    i++
+    console.log(item)
+   //  elemento inserito nel DOM
+    listElement.innerText += item
+   };
